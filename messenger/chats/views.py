5b68960b3d0e183cd88a6f33bfdd3a68cbfdb201
SarathK07 <sarathk0207@gmail.com>
2026-02-20 11:19:57 +0530

    updated some changes in front end and added screenshots

--- messenger/chats/views.py
+++ messenger/chats/views.py
@@ -2,10 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-
 from django.shortcuts import get_object_or_404
 import uuid
-
 from .models import Message, Group, GroupMember, GroupMessage
 from accounts.models import User
 
